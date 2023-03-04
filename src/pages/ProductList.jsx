77bff45f35e618b83b929dfee11fa83196e348a3
Nese84 <nbserbest84@gmail.com>
2023-03-04 13:38:42 +0300

    post

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,29 +1,33 @@
-
 import React,{useState,useEffect} from "react";
 import ProductCard from "../components/ProductCard";
 import CardTotal from "../components/CardTotal";
 import axios from "axios";
 
-
 const ProductList = () => {
   const url = process.env.REACT_APP_API_URL;
-
-  
   const [products,setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [loading,setLoading] = useState(true)
+  const [errorState,setErrorState] = useState(false)
  
   const getProducts = async () =>{
     console.log('merhaba');
     
     try {
+      setLoading(false)
       const { data } = await axios(url)
       setProducts(data)
-      setLoading(false)
+      setErrorState(false);
+      console.log(data);
       
     } catch (error) {
       console.log(error)
+      setErrorState(true)
     }
+
   }
+
+  console.log(products);
+  
 
   useEffect(() => {
     getProducts();
@@ -32,22 +36,29 @@ const ProductList = () => {
   return (
     <div className="container mt-3">
       <div className={"bg-light d-sm-block d-md-flex"}>
-       { loadaing ?  <p className="text-center text-danger w-100">Loading....</p>
+        {loading ? (
+          <p className="text-center text-danger w-100">Loading....</p>
+        ) : products.length > 0 ? (
+          <>
+            <article id="product-panel" className="col-md-5">
+              {products.map(item=>{
+                return <ProductCard key={item.id}  item={item} />;
+              }) }
+            </article>
+            <article className="col-md-5 m-3">
+              <CardTotal products={products} />
+            </article>
+          </>
+        ) : (
+          
+          !errorState && (
+            <p className="text-center text-danger w-100">No products data...</p>
+          )
+        )}
 
-       : products.length > 0 ?
-
-        <>
-          <article id="product-panel" className="col-md-5">
-            <ProductCard />
-          </article>
-          <article className="col-md-5 m-3">
-            <CardTotal />
-          </article>
-        </>
-
-        :
-
-        <p className="text-center text-danger w-100">No products data...</p> }
+        {errorState && (
+          <p className="text-center text-danger w-100">Error...</p>
+        )}
       </div>
     </div>
   );
