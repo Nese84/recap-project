31e5083751c1c057f76513ed71073910465da3a7
Nese84 <nbserbest84@gmail.com>
2023-03-04 13:04:01 +0300

    products card

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,56 @@
+
+import React,{useState,useEffect} from "react";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
+import axios from "axios";
+
+
+const ProductList = () => {
+  const url = process.env.REACT_APP_API_URL;
+
+  
+  const [products,setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+ 
+  const getProducts = async () =>{
+    console.log('merhaba');
+    
+    try {
+      const { data } = await axios(url)
+      setProducts(data)
+      setLoading(false)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="container mt-3">
+      <div className={"bg-light d-sm-block d-md-flex"}>
+       { loadaing ?  <p className="text-center text-danger w-100">Loading....</p>
+
+       : products.length > 0 ?
+
+        <>
+          <article id="product-panel" className="col-md-5">
+            <ProductCard />
+          </article>
+          <article className="col-md-5 m-3">
+            <CardTotal />
+          </article>
+        </>
+
+        :
+
+        <p className="text-center text-danger w-100">No products data...</p> }
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
